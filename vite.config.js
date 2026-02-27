3df7bec1ce1cefa4d903b9818fea6be5f75f5f36
AmanKumarGoyal <amankumar8080443@gmail.com>
2026-02-28 04:21:25 +0530

    Optimize build: increase chunk size limit and split Three.js bundles

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,23 @@ import * as path from 'path'
 
 export default defineConfig({
   root: 'src',
-  base: '/vite-threejs-template/', // for Github pages, otherwise use './'
+  base: './', // Changed for Vercel deployment
   build: {
     outDir: '../dist',
+    chunkSizeWarningLimit: 1000, // Increase limit to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'three': ['three'],
+          'three-addons': [
+            'three/examples/jsm/controls/OrbitControls.js',
+            'three/examples/jsm/objects/Reflector.js',
+            'three/examples/jsm/loaders/GLTFLoader.js',
+            'three/examples/jsm/exporters/GLTFExporter.js'
+          ]
+        }
+      }
+    }
   },
   server: {
     host: true, // to test on other devices with IP address
